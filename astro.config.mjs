003a82ec50dff3084c1951d98a5ee53825aa64af
Nahar Emet <naharemete@gmail.com>
2026-09-14 01:57:31 +0600

    chore: enforce trailing slashes for better SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://buildmy.house',
   output: 'static',
+  trailingSlash: 'always',
   integrations: [sitemap()],
   vite: {
     build: { target: 'es2022' },
