4ef0aef34b03be80b7e56ee6f7d9bc4877c6d8f9
Nahar Emet <naharemete@gmail.com>
2026-09-14 00:34:44 +0600

    chore: configure for Cloudflare Pages static hosting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  output: 'server',
-  adapter: cloudflare(),
+  output: 'static',
   vite: {
     build: { target: 'es2022' },
   },
